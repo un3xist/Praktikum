990e25fe340e9a6111bfd8c8d1a1fd88224feb87
Manuel Scholz <manuel@kurhotel-brandl.de>
2016-03-15 17:42:46 +0100

    Added and Completed AB1

--- src/AB1_1/HelloWorld.java
+++ src/AB1_1/HelloWorld.java
@@ -1,4 +1,4 @@
-package AB1;
+package AB1_1;
 
 public class HelloWorld {
 	public static void main(String[] args)
---
+++ src/AB1_2/HelloInformatik.java
@@ -0,0 +1,10 @@
+package AB1_2;
+
+public class HelloInformatik {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hallo, Manuel!\nInformatik ist mein Fach.\nProgrammieren macht Spaﬂ!");
+	}
+	
+}
