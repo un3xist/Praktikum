cceabeb29e2033962acbfae374a3aa5a2b6d25fb
Manuel Scholz <manuel@kurhotel-brandl.de>
2016-03-15 20:38:47 +0100

    Aufgabe 2.3 Erledigt

---
+++ src/AB2_3/Modulo.java
@@ -0,0 +1,32 @@
+/*Aufgabe 2.3: Modulo
+Schwerpunkte: Modulus
+Aufgabe: Schreiben Sie ein Programm, das vom Anwender zwei Zahlen a und b abfragt. Das Programm
+berechnet dann a modulo b und gibt das Ergebnis auf der Konsole aus. Die Berechnung soll auf zwei Arten
+erfolgen: Einmal mit dem Java-Operator und einmal mit der Formel, die hinter dem Operator steht. Beide
+Ergebnisse sollen ausgegeben werden.*/
+
+
+package AB2_3;
+import java.util.Scanner;
+
+public class Modulo {
+
+	public static void main(String[] args)
+	{
+		int a, b;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Bitte geben sie die erste Zahl an: ");
+		a = scanner.nextInt();
+		
+		System.out.println("Bitte geben sie die zweite Zahl an: ");
+		b = scanner.nextInt();
+		
+		scanner.close();
+		//                             Modulo                    Formel
+		System.out.println("Modulo: " + a%b + " Formel: " +  ( a-(a/b)*b ));
+		
+	}
+	
+}
