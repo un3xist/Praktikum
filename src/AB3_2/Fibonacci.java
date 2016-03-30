6d3152b2b9628324c7cc01eda86a11ea5e976e62
Manuel Scholz <manuel@kurhotel-brandl.de>
2016-03-30 15:08:36 +0200

    Fixed Formatting

--- src/AB3_2/Fibonacci.java
+++ src/AB3_2/Fibonacci.java
@@ -32,54 +32,53 @@ import java.util.Scanner;
 
 public class Fibonacci {
 
-  public static void main(String[] args) {
-    
-    
-    //Einen neuen scanner für die Eingabe erstellen
-    Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
 
-    System.out.println("Die wievielte Fibonacci Zahl soll ausgegeben werden?");
-    int fibonaccizahlen = scanner.nextInt();
-    
-    //Schließe den Scanner wieder.
-    scanner.close();
-    
-    //Zum berechnen brauchen wir zwei temporäre Variablen, welche die Fibonaccizaheln beim itterieren beinhalten
-    //long, da ein 32 Bit Integer nur bis zur 47. Fibonaccizahl möglich ist. long ist ein 64 bit Integer.
-    //Damit erreichen wir die 162. Zahl.
-    long f1 = 1;
-    long f2 = 1;
-    // Nun fangen wir mit der dritten Fibonaccizahl an (Wir kennen ja bereits die ersten zwei)
-   
-    int i = 3;
-    while (i <= fibonaccizahlen) {
-      long temp;
-    	//Zuerst speichern wir die letzte Fibonaccizahl zwischen
-      	temp = f2;
-      	//Nun erhoehen wir diese mit der vorrigen
-    	f2 = f1 + f2;
-    	//und setzen die vorrige auf den zwischengespeicherten Wert.
-    	f1 = temp;	
-    	i++;
-    }
-    
-    //Nun ist f2 die gewünschte Fibonaccizahl
-    
-    long gewuenschteFibonacciZahl = f2;
-    
-  
-    // Berechnung mittels der Formel von Moivre/Binet 
-    // fn =(phi^n-psi^n)/(phi-psi) 
-    // mit phi = (1+sqrt(5))/2 und psi = 1-phi 
-    double phi = (1 + Math.sqrt(5) )/2; 
-    double psi = 1-phi; double fibonacci =(Math.pow(phi, fibonaccizahlen) - Math.pow(psi, fibonaccizahlen) ) / (
-     phi - psi); 
-    
-    
+		// Einen neuen scanner für die Eingabe erstellen
+		Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Die " + fibonaccizahlen + ". Zahl betraegt: " + gewuenschteFibonacciZahl);
-    //Kontrollausgabe
-    System.out.println("Zur Kontrolle mit Moivre/Binet: " + fibonacci);
-    
-  }
+		System.out.println("Die wievielte Fibonacci Zahl soll ausgegeben werden?");
+		int fibonaccizahlen = scanner.nextInt();
+
+		// Schließe den Scanner wieder.
+		scanner.close();
+
+		// Zum berechnen brauchen wir zwei temporäre Variablen, welche die
+		// Fibonaccizaheln beim itterieren beinhalten
+		// long, da ein 32 Bit Integer nur bis zur 47. Fibonaccizahl möglich
+		// ist. long ist ein 64 bit Integer.
+		// Damit erreichen wir die 162. Zahl.
+		long f1 = 1;
+		long f2 = 1;
+		// Nun fangen wir mit der dritten Fibonaccizahl an (Wir kennen ja
+		// bereits die ersten zwei)
+
+		int i = 3;
+		while (i <= fibonaccizahlen) {
+			long temp;
+			// Zuerst speichern wir die letzte Fibonaccizahl zwischen
+			temp = f2;
+			// Nun erhoehen wir diese mit der vorrigen
+			f2 = f1 + f2;
+			// und setzen die vorrige auf den zwischengespeicherten Wert.
+			f1 = temp;
+			i++;
+		}
+
+		// Nun ist f2 die gewünschte Fibonaccizahl
+
+		long gewuenschteFibonacciZahl = f2;
+
+		// Berechnung mittels der Formel von Moivre/Binet
+		// fn =(phi^n-psi^n)/(phi-psi)
+		// mit phi = (1+sqrt(5))/2 und psi = 1-phi
+		double phi = (1 + Math.sqrt(5)) / 2;
+		double psi = 1 - phi;
+		double fibonacci = (Math.pow(phi, fibonaccizahlen) - Math.pow(psi, fibonaccizahlen)) / (phi - psi);
+
+		System.out.println("Die " + fibonaccizahlen + ". Zahl betraegt: " + gewuenschteFibonacciZahl);
+		// Kontrollausgabe
+		System.out.println("Zur Kontrolle mit Moivre/Binet: " + fibonacci);
+
+	}
 }
