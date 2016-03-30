94792444c909ea3ac2e0552980b2a4c65beda9b1
Manuel Scholz <manuel@kurhotel-brandl.de>
2016-03-30 15:48:09 +0200

    Grundgerüst von Aufgabe 4.2 gebaut

---
+++ src/AB4_2/Rennen.java
@@ -0,0 +1,32 @@
+/**
+ * Praktikum TIPTP1, SS 2016
+ * Gruppe: Manuel Scholz (manuel.scholz@haw-hamburg.de),
+ * Danny Luda (danny-marcel.luda@haw-hamburg.de)
+ * Aufgabe: Aufgabenblatt 4, Aufgabe 2
+ */
+
+/**
+ * Aufgabe 4.2: Rennen
+ * Schwerpunkte: Klasse mit Referenzen auf Objekte anderer Klassen
+ * Aufgabe: Schreiben Sie eine Klasse  Rennen . Ein  Rennen hat folgende Eigenschaften:
+ * •  Anzahl der  Rennautos  im  Rennen
+ * •  List der  Rennautos in einem Array
+ * •  Streckenlänge
+ * Implementieren Sie einen Konstruktor. Initialisieren Sie darin das Array mit der Länge 1. Die Klasse soll
+ * außerdem eine Methode  addRennauto(Rennauto) haben, die dem  Rennen ein zusätzliches  Rennauto
+ * hinzufügt. Falls im Array kein Platz mehr ist, soll die Arraylänge verdoppelt werden. Die bereits vorhandenen
+ * Rennautos dürfen dabei natürlich nicht verloren gehen. Ein  Rennen hat außerdem drei Methoden, um ein
+ * Rennen zu steuern:
+ * •  Rennauto ermittleSieger(): Liefert das  Rennauto , das bereits die gesamte Streckenlänge
+ * zurückgelegt hat. Falls dies für kein  Rennauto zutrifft, liefert sie  null . Falls mehrere  Rennautos
+ * bereits im Ziel sind, liefert sie ein beliebiges davon.
+ * •  void schritt(): Alle Teilnehmerinnen des  Rennens bewegen sich einen Schritt weiter.
+ * •  void durchfueren : Es wird so lange  schritt() aufgerufen, bis ein Sieger des  Rennens
+ * gefunden wurde.
+ */
+
+package AB4_2;
+
+public class Rennen {
+
+}
