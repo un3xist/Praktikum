c0a493e8f7f478151af5e52373413eb757edc049
Manuel Scholz <manuel@kurhotel-brandl.de>
2016-04-03 23:45:19 +0200

    * lok zu einer private Variablen gemacht * Methoden ausgearbeitet

--- src/AB5_2/Zug.java
+++ src/AB5_2/Zug.java
@@ -12,7 +12,7 @@ package AB5_2;
  */
 public class Zug {
 
-	Lokomotive lok;
+	private Lokomotive lok;
 
 	/**
 	 * Ein Zug ohne Wagen wird erstellt.
@@ -21,28 +21,56 @@ public class Zug {
 	 *            Eine Lokomotive.
 	 */
 	public Zug(Lokomotive lok) {
+		this.lok = lok;
 
 	}
 
 	/**
 	 * Haengt einen Wagen an das Ende des Zuges.
 	 * 
-	 * @param wagon
+	 * @param neuerWagen
 	 *            Ein Wagen, welcher an das Ende des Zugs gehaengt wird.
 	 */
-	public void wagenHinzufuegen(Wagen wagon) {
+	public void wagenHinzufuegen(Wagen neuerWagen) {
 
+		// Den ersten Wagen der Lokomotive holen
+		Wagen vordererWagen = lok.getErsterWagen();
+		// Wenn der erste Wagen null ist, muss dieser bei der Lok gesetzt werden
+		if (vordererWagen == null) {
+			lok.setErsterWagen(neuerWagen);
+		} else {
+			// Solange die Wagen durchgehen, bis der naechste null ist
+			while (vordererWagen.getNaechsterWagen() != null) {
+				vordererWagen = vordererWagen.getNaechsterWagen();
+			}
+			// naechsten Wagen setzen.
+			vordererWagen.setNaechsterWagen(neuerWagen);
+		}
 	}
 
 	/**
 	 * Entfernt den ersten Wagen des Zugs und gibt ihn zurueck. Die restlichen
-	 * Wagne ruecken auf. Falls kein Wagen im Zug ist, ist das Ergebnis null.
+	 * Wagen ruecken auf. Falls kein Wagen im Zug ist, ist das Ergebnis null.
 	 * 
-	 * @return Der erste Wagen des Zugs wird zurueck gegeben.
+	 * @return Der urspuenglich erste Wagen des Zugs wird zurueck gegeben.
 	 */
 	public Wagen erstenWagenEntfernen() {
 
-		return null;
+		// Da lediglich die Lokomotive ueber den ersten Wagen bescheid weiss,
+		// muss dort der erste auf den zweiten geandert werden und beim
+		// urpsurenglich ersten Wagen der zweite auf Null gesetzt werden.
+
+		if (lok.getErsterWagen() != null) {
+			Wagen urspruenglichErsterWagen = lok.getErsterWagen();
+			Wagen urspruenglichZweiterWagen = urspruenglichErsterWagen.getNaechsterWagen();
+
+			lok.setErsterWagen(urspruenglichZweiterWagen);
+			urspruenglichErsterWagen.setNaechsterWagen(null);
+
+			return urspruenglichErsterWagen;
+		} else {
+			return null;
+		}
 	}
 
 	/**
@@ -54,6 +82,18 @@ public class Zug {
 	 */
 	public void zugAnhaengen(Zug andererZug) {
 
+		// Solange den ersten Wagen entfernen und an diesen Zug anheangen
+		// zuerst pruefen, ob am Zug ueberhaupt andere Wagen sind
+
+		// die Wagenanzahlspruefung musste ich extra stellen, da in der for
+		// schleife die Anzahl dauernd neu gecheckt wird. Somit fehlt am schluss
+		// mindestens ein Wagen.
+		int wagenAnzahlAndererZug = andererZug.getWagenAnzahl();
+
+		for (int i = 0; i < wagenAnzahlAndererZug; i++) {
+			this.wagenHinzufuegen(andererZug.erstenWagenEntfernen());
+		}
+
 	}
 
 	/**
@@ -62,7 +102,14 @@ public class Zug {
 	 * @return Die Anzahl der Wagen ohne Lokomotive
 	 */
 	public int getWagenAnzahl() {
-		return 0;
+		int anzahlWagen = 0;
+		Wagen vordererWagen = this.lok.getErsterWagen();
+
+		while (vordererWagen != null) {
+			anzahlWagen++;
+			vordererWagen = vordererWagen.getNaechsterWagen();
+		}
+		return anzahlWagen;
 	}
 
 	/**
@@ -71,15 +118,31 @@ public class Zug {
 	 * @return Die gesammte Kapazitaet des Zuges.
 	 */
 	public int getKapazitaet() {
-		return 0;
+		int kapazitaet = 0;
+		Wagen vordererWagen = this.lok.getErsterWagen();
+
+		while (vordererWagen != null) {
+			kapazitaet += vordererWagen.getPassagierKapazitaet();
+			vordererWagen = vordererWagen.getNaechsterWagen();
+		}
+		return kapazitaet;
 	}
 
 	/**
 	 * Liefert gesammte Laenge des Zuges, Lokomotive und alle Wagen.
+	 * 
 	 * @return Die Gesamtlaenge des Zuges.
 	 */
 	public int getLaenge() {
-		return 0;
+		int laenge = 0;
+		laenge += lok.getLaenge();
+
+		Wagen vordererWagen = lok.getErsterWagen();
+		while (vordererWagen != null) {
+			laenge += vordererWagen.getLaenge();
+			vordererWagen = vordererWagen.getNaechsterWagen();
+		}
+		return laenge;
 	}
 
 	/**
@@ -91,6 +154,24 @@ public class Zug {
 	 * @return Die Beschreibung des Zuges.
 	 */
 	public String toString() {
-		return "hallo";
+		String ausgabeString;
+
+		ausgabeString = "Loktyp: " + this.lok.getTyp();
+		ausgabeString += ", Anzahl Wagen: " + this.getWagenAnzahl();
+		ausgabeString += ", Gesamtlaenge: " + getLaenge();
+
+		Wagen wagen = lok.getErsterWagen();
+		int i = 1;
+		while (wagen != null) {
+			ausgabeString += "; " + i + ". Wagen: ";
+			ausgabeString += "Seriennummer: " + wagen.getSeriennummer();
+			ausgabeString += ", Wagenlaenge: " + wagen.getLaenge();
+			ausgabeString += ", Passagierkapazitaet: " + wagen.getPassagierKapazitaet();
+
+			wagen = wagen.getNaechsterWagen();
+			i++;
+		}
+
+		return ausgabeString;
 	}
 }
