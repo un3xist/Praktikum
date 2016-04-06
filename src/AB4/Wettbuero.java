c0634a2b7c29523df8a44ca8fc210f321f65ff4c
Manuel Scholz <manuel@kurhotel-brandl.de>
2016-04-06 16:58:01 +0200

    Implementation des Wettbueros
    * ArrayList zur einfachen Verwaltung der Wetten
    * Auswertung der Wetten
    * Annahme neuer Wetten

--- src/AB4/Wettbuero.java
+++ src/AB4/Wettbuero.java
@@ -21,23 +21,92 @@
 
 package AB4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Wettbuero {
-  
-  int anzahlWetten;
-  double faktor;
-  
-  public Wettbuero(Rennen rennen, double gewinnfaktor)
-  {
-    
-  }
-  public void wetteAnnehmen(String fahrerin, int wetteinsatz, String spieler)
-  {
-    
-  }
-  
-  public void auswerten()
-  {
-    
-  }
+
+	int anzahlWetten;
+	double gewinnfaktor;
+	Rennen rennen;
+
+	List<Wette> wetten;
+
+	/**
+	 * Konstruktor der Klasse Wettbuero.
+	 * 
+	 * @param rennen
+	 *            Fuer welches Rennen das Wettbuero aufgemacht wird
+	 * @param gewinnfaktor
+	 *            Wie hoch der Gewinnfaktor der Wetten ist.
+	 */
+	public Wettbuero(Rennen autorennen, double wettGewinnfaktor) {
+		anzahlWetten = 0;
+		gewinnfaktor = wettGewinnfaktor;
+		rennen = autorennen;
+
+		wetten = new ArrayList<>();
+	}
+
+	/**
+	 * Nimmt eine Wette an und uebergibt sie in das Objekt Array.
+	 * 
+	 * @param fahrerin
+	 *            Auf welche Fahrerin die Wette geht.
+	 * @param wetteinsatz
+	 *            Wie hoch der Wetteinsatz ist.
+	 * @param spieler
+	 *            Welcher Spieler die Wette abgiebt.
+	 */
+	public void wetteAnnehmen(String fahrerin, int wetteinsatz, String spieler) {
+		Wette wette = new Wette(fahrerin, wetteinsatz, spieler);
+		wetten.add(wette);
+		anzahlWetten++;
+	}
+
+	/**
+	 * Prueft, ob ein Spieler gewonnnen hat und gibt das aus.
+	 */
+	public void auswerten() {
+		int anzahlSieger = 0;
+
+		Rennauto sieger = rennen.ermittleSieger();
+		// Check die Wetten nur, wenn es einen Sieger gibt.
+		if (sieger != null) {
+			// Schleife, die alle Wetten durchgeht
+			for (int i = 0; i < wetten.size(); i++) {
+
+				// Name des Siegers und Name auf den gewettet wurde
+				// zwischenspeichern, um dauernden Aufruf der Funktion zu
+				// vermeiden.
+				String rennsieger = sieger.getName();
+				String wettsieger = wetten.get(i).getFahrerin();
+
+				// Es wird genau auf die Schreibweise der Namen geachtet!
+				if (rennsieger.equals(wettsieger)) {
+					String gewinnString = "Spieler " + wetten.get(i).spieler + " hat "
+							+ wetten.get(i).wettEinsatz * gewinnfaktor + " gewonnen.";
+
+					System.out.println(gewinnString);
+					anzahlSieger++;
+				} else {
+					// Wenn die Wette nicht gewinnt
+					System.out.println(wetten.get(i).spieler + " hat leider nicht gewonnen.");
+				}
+
+			}
+
+		} 
+		//Zur uebersichtlichkeit kein else, sondern ein if
+		if (sieger == null){
+			System.out.println("Es gibt noch keinen Gewinner des Rennens.");
+		}
+		
+		//Ausgabe, wenn niemand richtig gewettet hat.
+		if (anzahlSieger == 0) {
+			System.out.println("Es gibt leider keinen Wettsieger.");
+		}
+
+	}
 
 }
